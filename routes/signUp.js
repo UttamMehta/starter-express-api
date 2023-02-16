fad4b2e97019de5b481f1a6807abb293b3505218
UttamMehta <binobajichiku@gmail.com>
2023-02-16 22:06:23 +0530

    Cylic work completed

--- routes/signUp.js
+++ routes/signUp.js
@@ -6,4 +6,10 @@ signUpRouter.post("/", register);
 signUpRouter.post("/logIn", login);
 signUpRouter.post("/appoint", appointment);
 
+signUpRouter.get("*", (req, res) => {
+  res.status(400).send({
+    message: "Wrong Search",
+  });
+});
+
 module.exports = signUpRouter;
