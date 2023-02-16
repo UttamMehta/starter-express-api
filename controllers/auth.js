91799b239f06408d509c4352aeb07c01d88308a3
UttamMehta <binobajichiku@gmail.com>
2023-02-17 01:16:39 +0530

    error remove sever side completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -117,7 +117,7 @@ async function singleUserAppointment(req, res) {
     if (userData.appointment === 0)
       return res.send({ message: "Not any appointment book" });
 
-    return res.send({ allAppointment: userData.appointment });
+    return res.send({ allAppointment: userData[0].appointment });
   } catch (err) {
     return res.status(500).send({
       error: "Server error please try later",
